9299dc0edff171ac28f6813f36e5a99ae92479a0
ZotovaIrina <ZotovaIrina@users.noreply.github.com>
2016-03-17 15:12:32 +0300

    Adding and update sub-documents to a document: create comment schema and add new comments to the dish

--- dataserver.js
+++ dataserver.js
@@ -2,18 +2,26 @@ var mongoose = require('mongoose'),
     assert = require('assert');
 
 var Dishes = require('./models/dishes');
+
 // Connection URL
-var url = 'mongodb://localhost:27017/conFusion';
-mongoose.connect(url);
+var url = 'mongodb://localhost:27017/conFusion';mongoose.connect(url);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     // we're connected!
     console.log("Connected correctly to server");
+
     // create a new dish
     Dishes.create({
         name: 'Uthapizza',
-        description: 'Test'
+        description: 'Test',
+        comments: [
+            {
+                rating: 3,
+                comment: 'This is insane',
+                author: 'Matt Daemon'
+            }
+        ]
     }, function (err, dish) {
         if (err) throw err;
         console.log('Dish created!');
@@ -31,13 +39,23 @@ db.once('open', function () {
                 new: true
             })
                 .exec(function (err, dish) {
-                    //get result of findByIdAndUpdate function and callback this function
                     if (err) throw err;
                     console.log('Updated Dish!');
                     console.log(dish);
 
-                    db.collection('dishes').drop(function () {
-                        db.close();
+                    dish.comments.push({
+                        rating: 5,
+                        comment: 'I\'m getting a sinking feeling!',
+                        author: 'Leonardo di Carpaccio'
+                    });
+
+                    dish.save(function (err, dish) {
+                        console.log('Updated Comments!');
+                        console.log(dish);
+
+                        db.collection('dishes').drop(function () {
+                            db.close();
+                        });
                     });
                 });
         }, 5000);
