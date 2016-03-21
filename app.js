0f9376b0f93f65dd5ba61c80caa84ab208a2827e
ZotovaIrina <ZotovaIrina@users.noreply.github.com>
2016-03-21 15:23:41 +0300

    Simple Authenticate

--- app.js
+++ app.js
@@ -30,6 +30,40 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+function auth (req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+    var err = new Error('You are not authenticated!');
+    err.status = 401;
+    next(err);
+    return;
+  }
+
+  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == 'admin' && pass == 'password') {
+    next(); // authorized
+  } else {
+    var err = new Error('You are not authenticated!');
+    err.status = 401;
+    next(err);
+  }
+}
+
+app.use(auth);
+
+app.use(function(err,req,res,next) {
+  res.writeHead(err.status || 500, {
+    'WWW-Authenticate': 'Basic',
+    'Content-Type': 'text/plain'
+  });
+  res.end(err.message);
+});
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -37,6 +71,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 
 app.use('/', routes);
 app.use('/users', users);
