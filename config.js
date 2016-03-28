89f253dd998430efb4956cab0d07ae9c4a0183d1
ZotovaIrina <ZotovaIrina@users.noreply.github.com>
2016-03-28 16:07:10 +0300

    https and facebook registration

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientID: 'YOUR FACEBOOK APP ID',
-        clientSecret: 'YOUR FACEBOOK SECRET',
+        clientID: '1840797509480907',
+        clientSecret: '95299af2c2bcd406357a0724bbb5b4f9',
         callbackURL: 'https://localhost:3443/users/facebook/callback'
     }
 };
